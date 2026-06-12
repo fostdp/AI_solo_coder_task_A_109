f9cd237c92b2238ff239bf6ed5288cbf86472d02
fostdp <129711106+fostdp@users.noreply.github.com>
2026-06-12 17:51:47 +0800

    Update project

---
+++ backend/Models/MigrationPredictionPoint.cs
@@ -0,0 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SculptureMonitor.Models
+{
+    public class MigrationPredictionPoint
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int MigrationPredictionId { get; set; }
+
+        [ForeignKey(nameof(MigrationPredictionId))]
+        public virtual MigrationPrediction MigrationPrediction { get; set; }
+
+        public double Depth { get; set; }
+
+        public double Concentration { get; set; }
+
+        public double? TimeStep { get; set; }
+
+        [Column(TypeName = "datetime")]
+        public DateTime? Timestamp { get; set; }
+    }
+}
